99751f5783123c280e8d9b57eee85521b15a560f
chaeeun604 <ce7828@ewha.ac.kr>
2025-06-21 01:16:09 +0900

    Update Outfit.java

--- 2차파일/Outfit.java
+++ 2차파일/Outfit.java
@@ -2,7 +2,8 @@ package TenTen;
 
 import java.util.*;
 
-public class Outfit { 
+// 코디 구성 객체 (상의/하의/액세서리)
+class Outfit {
     private ClothingItem top;
     private ClothingItem bottom;
     private List<Style> accessories;
@@ -10,14 +11,16 @@ public class Outfit {
     public Outfit(ClothingItem top, ClothingItem bottom, List<Style> accessories) {
         this.top = top;
         this.bottom = bottom;
-        this.accessories = accessories; 
+        this.accessories = accessories;
     }
 
+    // 코디 설명 제공
     public String getStyleExplanation() {
         return top.getCategory() + "과 " + bottom.getCategory() + "의 기본 스타일 조합입니다.";
     }
 
+    // 코디 정보 출력
     public void showOutfit() {
-        System.out.println("Top: " + top.getColor() + ", Bottom: " + bottom.getColor());
+        System.out.println("Top: " + top.getName() + ", Bottom: " + bottom.getName());
     }
 }
